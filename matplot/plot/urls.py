fa619c2ea8b1fc7f2a69a89a14935ccbfe2a2649
LMH <90649822+LoneHilke@users.noreply.github.com>
2023-01-20 21:32:14 +0100

    piechart virker
    Piechart virker, men /barchart har sammen problem som før

--- matplot/plot/urls.py
+++ matplot/plot/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns=[
-    path('barchart',views.barchart,name='barchart')
+    path('barchart',views.barchart,name='barchart'),
+    path('',views.piechart,name='piechart'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- matplot/plot/views.py
+++ matplot/plot/views.py
@@ -14,8 +14,28 @@ def barchart(request):
     plt.xticks(y_pos, objects)
     plt.ylabel('modul')
     plt.title('antal')
-    plt.savefig('image/barchart.png')
+    plt.savefig('image/barchart.png', dpi=100)
     return render(request,'barchart.html')
+
+
+
+
+# Pie Chart
+def piechart(request):
+    # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+    labels = 'Sale', 'Purchase'
+    #labels = 'morgen','eftermiddag','ferie'
+    sizes = [random.randint(10,30), random.randint(30,50)]
+    explode = (0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+    #explode = (29,46,39)
+
+    fig1, ax1 = plt.subplots()
+    
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.savefig('image/sale_purchase_peichart.png',dpi=100)
+    return render(request,'piechart.html')
 
 
 
