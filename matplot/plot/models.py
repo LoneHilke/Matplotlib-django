75d5e82a53fa88c97936edd33f8997f7fdc326d1
LMH <90649822+LoneHilke@users.noreply.github.com>
2025-05-26 21:59:05 +0200

    opdateret med model
    Nu med model

--- matplot/plot/models.py
+++ matplot/plot/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from django_matplotlib import MatplotlibFigureField
 
-# Create your models here.
+class MyModel(models.Model):
+    figure = MatplotlibFigureField(figure='my_figure')
